7989b67c89e4653444bfe8042efc1f86ca6fe9b5
James Marsh <mrshnja@amazon.com>
2022-03-04 20:23:48 +0000

    the Mach-O assembler doesn't have some directives and requires underscores

--- core/shared/platform/tvos/shared_platform.cmake
+++ core/shared/platform/tvos/shared_platform.cmake
@@ -3,7 +3,7 @@
 
 set (PLATFORM_SHARED_DIR ${CMAKE_CURRENT_LIST_DIR})
 
-add_definitions(-DBH_PLATFORM_DARWIN)
+add_definitions(-DBH_PLATFORM_TVOS)
 
 include_directories(${PLATFORM_SHARED_DIR})
 include_directories(${PLATFORM_SHARED_DIR}/../include)
@@ -11,6 +11,9 @@ include_directories(${PLATFORM_SHARED_DIR}/../include)
 include (${CMAKE_CURRENT_LIST_DIR}/../common/posix/platform_api_posix.cmake)
 
 file (GLOB_RECURSE source_all ${PLATFORM_SHARED_DIR}/*.c)
+
+# Some syscalls (e.g sigaltstack) are not available on tvOS, so we have a custom implementation of posix_thread.c
+list(FILTER PLATFORM_COMMON_POSIX_SOURCE EXCLUDE REGEX ".*posix_thread.c")
 
 set (PLATFORM_SHARED_SOURCE ${source_all} ${PLATFORM_COMMON_POSIX_SOURCE})
 
