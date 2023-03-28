32d5b5d313587c9f8752e632d655acf00fce7bd6
Marcin Kolny <mkolny@amazon.com>
2023-03-28 10:34:57 +0100

    Add test for validating linear memory size updates
    When memory.grow operation is called, it updates a `linear_mem_size` variable
    in the `wasm_interp_call_func_bytecode()`; that update is not propagated to
    all the threads, therefore if the function already is running and want to
    access the memory that was added (after `memory.grow`) e.g. by calling
    `memory.copy` where we check boundaries, the `memory.copy` fails with:
    `Exception: out of bounds memory access` exception. I'm adding a test for now,
    but will work on the implementation later.
    
    In the meantime, suggestions on how to approach this problem are very welcome

--- core/iwasm/libraries/lib-wasi-threads/test/build.sh
+++ core/iwasm/libraries/lib-wasi-threads/test/build.sh
@@ -12,6 +12,12 @@ WAMR_DIR=../../../../..
 for test_c in *.c; do
     test_wasm="$(basename $test_c .c).wasm"
 
+    if [ $test_wasm = "linear_memory_size_update.wasm" ]; then
+        thread_start_file=""
+    else
+        thread_start_file=$WAMR_DIR/samples/wasi-threads/wasm-apps/wasi_thread_start.S
+    fi
+
     echo "Compiling $test_c to $test_wasm"
     $CC \
         $WASI_SYSROOT_OPTION \
@@ -25,6 +31,6 @@ for test_c in *.c; do
         -Wl,--export=malloc \
         -Wl,--export=free \
         -I $WAMR_DIR/samples/wasi-threads/wasm-apps \
-        $WAMR_DIR/samples/wasi-threads/wasm-apps/wasi_thread_start.S \
+        $thread_start_file \
         $test_c -o $test_wasm
 done
